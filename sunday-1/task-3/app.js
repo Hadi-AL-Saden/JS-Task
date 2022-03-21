df7206207f5390046e1e461667cee7ee86cfde04
Hadi-AL-Saden <hadialsaden@gmail.com>
2022-03-20 23:06:27 -0700

    js

---
+++ sunday-1/task-3/app.js
@@ -0,0 +1,38 @@
+let t1=prompt("pick one plz (-,*,+,/)");
+let t2=prompt("first num");
+let t3=prompt("second num");
+
+
+let t_2=Number(t2);
+let t_3=Number(t3);
+
+if(t2+t3){
+     console.log(t_2+t_3);    
+// document.getElementById("result=").innerHTML
+}
+else{
+    console.log("nothing to print");
+}
+
+if (t_2-t_3){
+console.log(t_2-t_3);
+// document.getElementById("result=").innerHTML
+}
+else{
+    console.log("nothing to print");
+} 
+if (t_2*t_3){
+console.log(t_2*t_3);
+// document.getElementById("result=").innerHTML
+}
+else{
+    console.log("nothing to print");
+} 
+if (t_2/t_3){
+console.log(t_2/t_3);
+// document.getElementById("result=").innerHTML
+}
+else{
+    console.log("nothing to print");
+} 
+
