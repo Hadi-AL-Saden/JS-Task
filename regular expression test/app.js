cfb0ea977a0bdc320a6715c25daee0ccc21381af
Hadi-AL-Saden <hadialsaden@gmail.com>
2022-03-30 21:48:58 -0800

    js

---
+++ regular expression test/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// let H="Hadi - AL-Saden",
+// result = String.search(/L/);
+// console.log(result);
+
+
+let text="ahmad went to aqaba Ahmad eated"
+
+let result=/ahmed/i;
+let patren=text.match(result);
+
+document.write(patren);
